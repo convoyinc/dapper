8fd321168628710c1e0b922fbda63569813b6f04
Rylan Hawkins <RylanH@gmail.com>
2017-02-17 21:53:28 -0800

    Support for padding/marginHorizontal/Vertical

---
+++ src/plugins/horizontalVertical.ts
@@ -0,0 +1,24 @@
+import { Style } from '../types';
+
+export default function horizontalVertical(style: Style) {
+  for (const property in style) {
+    if (property === 'paddingHorizontal') {
+      style['paddingLeft'] = style[property];
+      style['paddingRight'] = style[property];
+      delete style[property];
+    } else if (property === 'paddingVertical') {
+      style['paddingTop'] = style[property];
+      style['paddingBottom'] = style[property];
+      delete style[property];
+    } else if (property === 'marginHorizontal') {
+      style['marginLeft'] = style[property];
+      style['marginRight'] = style[property];
+      delete style[property];
+    } else if (property === 'marginVertical') {
+      style['marginTop'] = style[property];
+      style['marginBottom'] = style[property];
+      delete style[property];
+    }
+  }
+  return style;
+}
