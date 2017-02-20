9c1463511ffabeeeb15f4d82abd8f16f6ae56557
Rylan Hawkins <rylan@convoy.com>
2017-02-20 14:50:31 -0800

    Fix typings when importing

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,8 @@ set -e
 
 source ./scripts/include/node.sh
 
+npm run compile
+
 PACKAGE_XY=$(node -e "console.log(JSON.parse(fs.readFileSync('package.json')).version.replace(/\.\d+$/, ''))")
 PACKAGE_VERSION="${PACKAGE_XY}.${CIRCLE_BUILD_NUM}"
 
