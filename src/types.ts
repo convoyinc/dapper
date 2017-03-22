1725c977a39e11399fff02db772f0e16767c3706
Ian MacLeod <ian@nevir.net>
2017-03-22 10:07:14 -0700

    Document StyleRule and StyleDeclaration

--- src/types.ts
+++ src/types.ts
@@ -14,9 +14,17 @@ export type ClassNameResolver = {[key: string]: boolean};
 
 export type StyleReducer = (modes: ClassNameResolver) => string;
 
-export type StyleRule = {[key: string]: number|string|string[]|StyleRule};
-
+/**
+ * A collection of style rules represented by JavaScript Objects, which are
+ * eventually flattened, and compiled down into actual CSS rules.
+ */
 export type StyleDeclaration = {[key: string]: StyleRule};
+
+/**
+ * An individual style rule, containing CSS property declarations (with JS-style
+ * property names), as well as other nested rules.
+ */
+export type StyleRule = {[key: string]: number|string|string[]|StyleRule};
 
 export type ComputedStyleSheet<Keys extends string> = {
   [Key in Keys]: ComputedStyle;
