3909f93aa2c5ed855851d347b46911a7705f81d0
Rylan Hawkins <RylanH@gmail.com>
2017-03-22 22:36:33 -0700

    Get horizontalVertical plugin to run on nested rules

--- src/plugins/horizontalVertical.ts
+++ src/plugins/horizontalVertical.ts
@@ -1,8 +1,11 @@
 import { StyleRule } from '../types';
 
-export default function horizontalVertical(style: StyleRule) {
+export default function horizontalVertical(style: StyleRule): StyleRule {
   for (const property in style) {
-    if (property === 'paddingHorizontal') {
+    const cssValue = style[property];
+    if (cssValue instanceof Object && !Array.isArray(cssValue)) {
+      style[property] = horizontalVertical(cssValue as StyleRule);
+    } else if (property === 'paddingHorizontal') {
       style['paddingLeft'] = style[property];
       style['paddingRight'] = style[property];
       delete style[property];
