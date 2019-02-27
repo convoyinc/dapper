1320a87258d66b0714de9d18fe95d23925c580fa
Pat Finnigan <pfinnigan@convoy.com>
2019-02-27 07:45:14 -0800

    add unit test for omitUniqueSuffices

--- test/unit/configure.ts
+++ test/unit/configure.ts
@@ -2,6 +2,7 @@ import * as sinon from 'sinon';
 import * as proxyquire from 'proxyquire';
 import configure from '../../src/configure';
 import { resetUniqueId } from '../../src/libs/generateClassName';
+import keyframes from '../../src/keyframes';
 
 proxyquire.noCallThru();
 
@@ -22,6 +23,7 @@ describe(`configure`, () => {
 
   afterEach(() => {
     sandbox.restore();
+    configure({ omitUniqueSuffices: false });
   });
 
   it(`friendlyClassNames set to false doesn't add name`, () => {
@@ -35,5 +37,43 @@ describe(`configure`, () => {
     });
     expect(className).to.deep.equal({root: 'dapper-a'});
     expect(renderCSSTextStub).to.have.been.calledWith(['.dapper-a{background-color:red;color:blue}']);
+  });
+
+  it(`omitUniqueSuffices omits unique sufficies for class names and keyframes`, () => {
+    const fadeOut1 = keyframes({
+      '0%': {
+        opacity: 0,
+      },
+      '100%': {
+        opacity: 1,
+      },
+    });
+    const className1 = compile({
+      root: {
+        backgroundColor: 'red',
+        color: 'blue',
+      },
+    });
+    expect(fadeOut1).to.equal('dapper-anim-1');
+    expect(className1).to.deep.equal({root: 'dapper-a'});
+
+    configure({ omitUniqueSuffices: true });
+
+    const fadeOut2 = keyframes({
+      '0%': {
+        opacity: 0,
+      },
+      '100%': {
+        opacity: 1,
+      },
+    });
+    const className2 = compile({
+      root: {
+        backgroundColor: 'red',
+        color: 'blue',
+      },
+    });
+    expect(fadeOut2).to.equal('dapper-anim-');
+    expect(className2).to.deep.equal({root: 'dapper-'});
   });
 });
